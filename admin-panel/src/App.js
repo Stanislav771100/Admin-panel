2364879fa30275291f10ca3658603ce85714aee1
Stanislav771100 <shtanko.stanislav@gmail.com>
2020-10-10 14:24:31 +0300

    fix width

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Leftbar from './screens/Leftbar/Leftbar';
 import Header from './screens/Header/Header';
+import HomeWork from './screens/HomeWork/HomeWork';
 
 function App() {
   return (
     <div className="App">
         <Leftbar />
-        <Header />
+        <div className="main">
+          <Header />
+          <HomeWork />
+        </div>
     </div>
   );
 }
